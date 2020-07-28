3078ef4a4341f90b28f499b5eabece6d163cbef8
Peter Sammon <petersammon2@gmail.com>
2020-07-28 00:35:51 -0400

    initialized search component and rendered onto page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import Employees from './components/Employees'
+import Search from './components/Search'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App" >
         <Header />
+        <Search />
         <Employees />
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
 
 const Header = () => {
     return (
@@ -15,10 +12,6 @@ const Header = () => {
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#link">Link</Nav.Link>
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
             </Navbar.Collapse>
         </Navbar>
     )
