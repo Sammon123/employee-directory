5d90eb32b523999d920a16c487d11d66c8fc3fb8
Peter Sammon <petersammon2@gmail.com>
2020-07-28 00:28:15 -0400

    displaying employee information onto the page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import Employees from './components/Employees'
 import './App.css';
 
 class App extends Component {
-  state = {
-    employee: [
-      {
-        id: 1,
-        name: 'Peter Sammon',
-        title: 'associate',
-        hours: 'day shift',
-        type: 'full time',
-        benefits: 'Medical'
-      },
-      {
-        id: 2,
-        name: 'John Doe',
-        title: 'associate',
-        hours: 'evening shift',
-        type: 'full time',
-        benefits: 'none'
-      },
-      {
-        id: 3,
-        name: 'Dave "Larry" Allen',
-        title: 'associate',
-        hours: 'night shift',
-        type: 'part time',
-        benefits: 'Medical, Dental'
-      }
-    ]
-  }
-
   render() {
-    const employees = this.state.employee.map((id, name) => {
-      <li key={employee.id}>{employee.name}</li>
-    })
     return (
       <div className="App" >
         <Header />
-        <ul>
-          {this.employee}
-        </ul>
-    );
+        <Employees />
       </div>
+    );
   }
 }
 
---
+++ src/components/Employees.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export class Employees extends Component {
+    state = {
+        employee: [
+            {
+                id: 1,
+                name: 'Peter Sammon',
+                title: 'associate',
+                hours: 'day shift',
+                type: 'full time',
+                benefits: 'Medical'
+            },
+            {
+                id: 2,
+                name: 'John Doe',
+                title: 'associate',
+                hours: 'evening shift',
+                type: 'full time',
+                benefits: 'none'
+            },
+            {
+                id: 3,
+                name: 'Dave "Larry" Allen',
+                title: 'associate',
+                hours: 'night shift',
+                type: 'part time',
+                benefits: 'Medical, Dental'
+            }
+        ]
+    }
+
+
+    render() {
+        return this.state.employee.map((employee) => (
+            <ul>
+                <h1>{employee.name}</h1>
+                <li>{employee.title}</li>
+                <li>{employee.hours}</li>
+                <li>{employee.type}</li>
+                <li>{employee.benefits}</li>
+            </ul>
+        ))
+    }
+}
+
+export default Employees
