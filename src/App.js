2c722324732b8ccc5c5cb4d904c7e994a948295f
Peter Sammon <petersammon2@gmail.com>
2020-07-27 23:26:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header'
 import './App.css';
 
-function App() {
-  // state = {
-  //   employee: [
-  //     {
-  //       id: 1,
-  //       name: 'Peter Sammon',
-  //       title: 'associate',
-  //       hours: 'day shift',
-  //       type: 'full time',
-  //       benefits: 'Medical'
-  //     },
-  //     {
-  //       id: 2,
-  //       name: 'John Doe',
-  //       title: 'associate',
-  //       hours: 'evening shift',
-  //       type: 'full time',
-  //       benefits: 'none'
-  //     },
-  //     {
-  //       id: 3,
-  //       name: 'Dave "Larry" Allen',
-  //       title: 'associate',
-  //       hours: 'night shift',
-  //       type: 'part time',
-  //       benefits: 'Medical, Dental'
-  //     }
-  //   ]
-  // }
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  state = {
+    employee: [
+      {
+        id: 1,
+        name: 'Peter Sammon',
+        title: 'associate',
+        hours: 'day shift',
+        type: 'full time',
+        benefits: 'Medical'
+      },
+      {
+        id: 2,
+        name: 'John Doe',
+        title: 'associate',
+        hours: 'evening shift',
+        type: 'full time',
+        benefits: 'none'
+      },
+      {
+        id: 3,
+        name: 'Dave "Larry" Allen',
+        title: 'associate',
+        hours: 'night shift',
+        type: 'part time',
+        benefits: 'Medical, Dental'
+      }
+    ]
+  }
+
+  render() {
+    const employees = this.state.employee.map((id, name) => {
+      <li key={employee.id}>{employee.name}</li>
+    })
+    return (
+      <div className="App" >
+        <Header />
+        <ul>
+          {this.employee}
+        </ul>
+    );
+      </div>
+  }
 }
 
 export default App;
